e7004682c414aee57ae4e21981dfe6e2c105cea3
ocavue <ocavue@gmail.com>
2019-02-09 14:01:42 +0800

    refactor: Move home page into a separate file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'kana.dart';
+import 'home_page.dart';
 
 void main() => runApp(App());
 
@@ -10,83 +10,6 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'Project Kana',
       home: HomePage(title: 'Project Kana'),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  HomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  Widget _buildGridView() {
-    List<Widget> gridCells = [];
-
-    for (final kana in kanas) {
-      final cell = InkWell(
-        child: Container(
-          child: Column(
-            children: [
-              Text(
-                kana.hiragana,
-                style: TextStyle(fontSize: 24.0),
-              ),
-              Row(
-                children: [
-                  Text(kana.katakana),
-                  Text(kana.romaji),
-                ],
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-              )
-            ],
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-          ),
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.black12, width: 0.5),
-          ),
-        ),
-        onTap: () {
-          print(kana);
-        },
-      );
-      gridCells.add(cell);
-    }
-
-    return GridView.count(
-      primary: false,
-      crossAxisSpacing: 0,
-      mainAxisSpacing: 0,
-      crossAxisCount: 5,
-      children: gridCells,
-      padding: EdgeInsets.only(bottom: 64), // Add padding for FAB
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.settings),
-            tooltip: 'Setting',
-            onPressed: () => print('Setting'),
-          ),
-        ],
-      ),
-      body: _buildGridView(),
-      floatingActionButton: FloatingActionButton.extended(
-        icon: Icon(Icons.check),
-        label: Text("Start Quiz"),
-        onPressed: () => print('Quiz'),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
