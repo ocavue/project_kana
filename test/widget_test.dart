c5c9e6f0e2c3ef6cb6e1f48ca10b8ab712afd3b3
ocavue <ocavue@gmail.com>
2019-02-18 00:13:39 +0800

    chore: Use Cirrus CI to test and build

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,22 +9,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:project_kana/main.dart';
+import 'package:project_kana/kana.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Home page smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(App());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text(kanas[0].hiragana), findsOneWidget);
+    expect(find.text(kanas[4].hiragana), findsOneWidget);
+    expect(find.text('settings'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    await tester.tap(find.byIcon(Icons.settings));
+    await tester.pumpAndSettle();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text(kanas[0].hiragana), findsNothing);
+    expect(find.text(kanas[4].hiragana), findsNothing);
+    expect(find.text('Settings'), findsOneWidget);
   });
 }
