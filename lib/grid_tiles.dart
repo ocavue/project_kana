7e9ae994924b6c44e17156f001502671e943ad3b
ocavue <ocavue@gmail.com>
2019-03-06 22:23:05 +0800

    feat: Update grid title style

---
+++ lib/grid_tiles.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+import 'kana.dart';
+import 'kana_dialog.dart';
+
+class EmptyGridTile extends StatelessWidget {
+  final double gridSpacing;
+
+  EmptyGridTile(this.gridSpacing) : super();
+
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: new BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey[400],
+            blurRadius: 0.0,
+            spreadRadius: gridSpacing,
+          )
+        ],
+        color: Theme.of(context).scaffoldBackgroundColor,
+      ),
+    );
+  }
+}
+
+class KanaGridTile extends StatelessWidget {
+  final double gridSpacing;
+  final Kana kana;
+
+  KanaGridTile(this.gridSpacing, this.kana) : super();
+
+  Widget build(BuildContext context) {
+    return Container(
+      child: Ink(
+        decoration: new BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey[400],
+              blurRadius: 0.0,
+              spreadRadius: gridSpacing,
+            )
+          ],
+          color: Theme.of(context).scaffoldBackgroundColor,
+        ),
+        child: InkWell(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    kana.romaji,
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(kana.hiragana, style: TextStyle(fontSize: 24.0)),
+                  Text(kana.katakana, style: TextStyle(fontSize: 24.0)),
+                ],
+              ),
+            ],
+          ),
+          onTap: () {
+            KanaDialog(kana).show(context);
+          },
+        ),
+      ),
+    );
+  }
+}
